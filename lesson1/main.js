5313d48eb50a474f06a716be4154f98a569f184b
ttshangxiang <ttshangxiang@qq.com>
2019-07-31 00:28:08 +0800

    '.'

---
+++ lesson1/main.js
@@ -0,0 +1,33 @@
+// 场景
+const scene = new THREE.Scene()
+// 相机
+const camera = new THREE.PerspectiveCamera(
+  75, window.innerWidth / window.innerHeight, 0.1, 1000
+)
+// 渲染器
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+// 形状
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+// 素材
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+// 物体 = 形状 + 素材
+const cube = new THREE.Mesh(geometry, material)
+// 添加物体
+scene.add(cube)
+// 设置摄像机高度
+camera.position.z = 10
+// 渲染方法
+function render () {
+  // 动画方法，60帧递归调用
+  requestAnimationFrame(render)
+  // cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  // 渲染器执行
+  renderer.render(scene, camera)
+}
+// 执行渲染方法
+render()
